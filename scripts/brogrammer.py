7843a5eb7fc01db3299a892e77d98d2644210a2c
Jarred <jparr721@gmail.com>
2017-12-20 19:34:16 -0500

    Some script kiddie shit

---
+++ scripts/brogrammer.py
@@ -0,0 +1,46 @@
+import os, platform
+if platform.system() == "Windows":
+    import msvcrt # In case I wanna run this on windows
+
+def menu():
+    os.chdir("/")
+    print("Current directory: "+os.getcwd())
+    print("Brogrammer, the environment setup system")
+    print("1 - Prep Linux Zone")
+    opt = input("Select an option: ")
+
+    if opt == "1":
+        fresh = input("Fresh install? (y/n): ")
+        fresh = True if fresh == "y" or fresh == "Y" else False
+        prepare_linux_zone(fresh)
+
+# Prepares the zone to be coded on, if fresh, install dependencies.
+# Also it assumes atom is default editor. Personal preference.
+def prepare_linux_zone(fresh):
+    if platform.system() == "Linux" and not fresh:
+        os.system("atom &")
+    elif platform.system() == "Linux" and fresh:
+        switch = input("Change direcotories? (y/n): ")
+        clone = input("Download any github repositories? (y/n): ")
+        if (switch == "y" or switch == "Y"):
+            dirpath = input("What directory? (absolute): ")
+            os.chdir(dirpath)
+        while (clone != "n" or clone != "N"):
+            repo = input("Enter a repo name: (Case Sensitive): ")
+            user = input("Enter the user name: (Case Sensitive): ")
+            https = True
+            delivery = input("https or ssh? (https/ssh): ")
+            https = True if delivery = "https" else False
+            if (repo is not None and https):
+                print("Cloning with https...")
+                os.system("git clone https://github.com/"+user+"/"+repo+".git")
+                print("Done")
+            elif (repo is not None and not https):
+                print("Cloning with ssh...")
+                os.system("git clone git@github.com:"+user+"/"+repo+".git")
+                print("Done")
+            clone = input("Download any github repositories? (y/n): ")
+        os.system("atom &")
+
+if __name__ == "__main__":
+    menu()
