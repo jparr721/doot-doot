3962b629764b169fa35be175588f73487846a2d9
jparr721 <jparr721@gmail.com>
2018-02-17 16:55:58 -0500

    Re-dump

---
+++ .config/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then 
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload mybar &
+	done
+else
+	polybar --reload mybar &
+fi
