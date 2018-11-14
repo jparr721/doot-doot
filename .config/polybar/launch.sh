748d87b2a0f5fdbac54ed3df0ce117bc7ce19f26
Jarred Parr <jparr721@gmail.com>
2018-11-14 09:10:26 -0500

    commit new polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,10 +4,14 @@ killall -q polybar
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then 
+if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		MONITOR=$m polybar --reload mybar &
+    MONITOR=$m polybar --reload main-bottom-tray &
+    MONITOR=$m polybar --reload main-bottom &
 	done
 else
 	polybar --reload mybar &
+  polybar --reload main-bottom-tray &
+  polybar --reload main-bottom &
 fi
