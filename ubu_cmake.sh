6722f50b44cea958f825ba41e530f61504aaf535
Jarred Parr <jparr721@gmail.com>
2021-10-04 19:05:39 -0600

    Add a setup script

---
+++ ubu_cmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euox pipefail
+
+sudo apt remove --purge --auto-remove cmake && \
+sudo apt update && \
+sudo apt install -y software-properties-common lsb-release && \
+sudo apt clean all && \
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null && \
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" && \
+
+sudo apt update && \
+sudo apt install kitware-archive-keyring && \
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg && \
+sudo apt update && sudo apt install -y cmake
+
+sudo apt install -y ninja-build
+sudo apt install -y libxinerama-dev
+sudo apt install -y libxcursor-dev
+sudo apt install -y libxi-dev
