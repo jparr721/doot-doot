9281bb8edb09645b364b9ad9fed9bff4d0fcc7e8
Jarred Parr <jparr721@gmail.com>
2022-01-01 12:25:48 -0700

    adding anaconda

--- setup.sh
+++ setup.sh
@@ -17,3 +17,8 @@ sudo apt install -y fd-find && sudo ln -s $(which fdfind) $HOME/.local/bin/fd
 echo "Getting node"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install 16.13.1 && nvm use 16.13.1
+
+echo "Getting anaconda"
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
+chmod +x Anaconda3-2021.05-Linux-x86_64.sh && sh -c Anaconda3-2021.05-Linux-x86_64.sh
