d77314499bdc402897ce505932e55146d59f8035
Jarred Parr <jparr721@gmail.com>
2025-11-20 09:44:47 -0700

    fix kali docker

--- install-docker-kali.sh
+++ install-docker-kali.sh
@@ -12,7 +12,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | \
+  bookworm stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
