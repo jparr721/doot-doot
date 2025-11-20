e21416192f9bffb0c6271a603195a787be2f0cf7
Jarred Parr <jparr721@gmail.com>
2025-11-19 19:47:17 -0700

    way better vim config

---
+++ install-luals.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euox pipefail
+
+echo "install luals"
+
+cd /tmp
+curl -LO https://github.com/LuaLS/lua-language-server/releases/download/3.15.0/lua-language-server-3.15.0-linux-x64.tar.gz
+sudo rm -rf /opt/lua-language-server
+sudo mkdir -p /opt/lua-language-server
+sudo tar -C /opt/lua-language-server -xzf lua-language-server-3.15.0-linux-x64.tar.gz
+sudo ln -sf /opt/lua-language-server/bin/lua-language-server /usr/bin/lua-language-server
+sudo mkdir -p /opt/lua-language-server/log/cache
+sudo chown -R $USER:$USER /opt/lua-language-server/log
--- install-neovim.sh
+++ install-neovim.sh
@@ -5,8 +5,10 @@ set -euox pipefail
 echo "install neovim"
 
 cd /tmp
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim-linux-x86_64
+# TODO - Switch back once 12 releases
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64 /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 sudo ln -sf /opt/nvim/bin/nvim /usr/bin/nvim
